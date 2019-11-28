48ac7d835fe487ac0ff4ba5d36935e76b592ae9d
realYazi <47607500+realYazi@users.noreply.github.com>
2019-11-28 20:27:49 +0000

    Update Bike.java

--- src/uk/ac/ed/bikerental/Bike.java
+++ src/uk/ac/ed/bikerental/Bike.java
@@ -3,22 +3,30 @@ package uk.ac.ed.bikerental;
 import java.math.BigDecimal;
 import java.util.TreeSet;
 
+enum BikeStatus {
+	AVAILABLE,
+	RESERVED,
+	CHECKED_OUT,
+	ON_DELICERY;
+}
+
 public class Bike {
     /* fields */
     private BikeProvider bikeProvider;
     private BikeType bikeType;
-    private BigDecimal replacementValue;
-    public Boolean checkedIn;
+    // private BigDecimal replacementValue; moved to BikeType class
+    // public Boolean checkedIn; combined into BikeStatus
     public TreeSet<DateRange> bookings;
-
+    public BikeStatus status;  // there are 4 statuses for a bike
 
 
     /* constructor */
     Bike(BikeType bikeType,  BikeProvider bikeProvider, BigDecimal replacementValue) {
         this.bikeProvider = bikeProvider;
         this.bikeType = bikeType;
-        this.replacementValue = replacementValue;
-        this.checkedIn = true;
+        // this.replacementValue = replacementValue;
+        // this.checkedIn = true;
+        this.status = BikeStatus.AVAILABLE;
         this.bookings = new TreeSet<DateRange>();
     }
 
@@ -27,10 +35,11 @@ public class Bike {
 
     public BikeType getBikeType() {return this.bikeType;}
 
-    public BigDecimal getReplacementValue() {return this.replacementValue;}
+    // public BigDecimal getReplacementValue() {return this.replacementValue;}
 
-    public Boolean isCheckedIn() {return this.checkedIn;}
-
+    // public Boolean isCheckedIn() {return this.checkedIn;}
+    public Boolean isAvailable() {return this.status == BikeStatus.AVAILABLE;}
+    
     // TODO test functionality (do we even need this)
     public BigDecimal getDepositValue(BigDecimal depositRate) {
         return depositRate.multiply(this.getType().getReplacementValue());
@@ -39,16 +48,16 @@ public class Bike {
     public TreeSet<DateRange> getBookings() {return this.bookings;}
 
     /* methods */
-    public void checkOut() {this.checkedIn = false;}
+    public void checkOut() {this.status = BikeStatus.CHECKED_OUT;}
 
-    public void checkIn() {this.checkedIn = true;}
+    public void checkIn() {this.status = BikeStatus.AVAILABLE;}
 
     public Boolean checkDateRange(DateRange) {}
         // TODO check if bike is avaliable to book over a specified date range
 
-    public void book(DateRange) {}
+    public void book(DateRange) {
         // TODO add dateRange to treemap
-
-
-
+        this.status = BikeStatus.RESERVE;
+    }
+    
 }
