ab3df198a1706f68db6c95a0bc2e24be00bf2d19
realYazi <47607500+realYazi@users.noreply.github.com>
2019-11-28 15:02:19 +0000

    Update BikeProvider.java

--- src/uk/ac/ed/bikerental/BikeProvider.java
+++ src/uk/ac/ed/bikerental/BikeProvider.java
@@ -1,28 +1,70 @@
 package uk.ac.ed.bikerental;
 
 public class BikeProvider {
-
     /* fields */
     private String name;
     private Location location;
     private ArrayList<Bike> bikeStock;
-    private Boolean partnershipAgreement;
-
+    private HashSet<BikeProvider> partners;
+    private String phoneNumber;
+    
     /* constructor */
-    BikeProvider(String name, ArrayList<Bike> bikeStock, Boolean partnershipAgreement) {
+    /**
+     * A bike provider registers onto the system by providing the following:
+     * @param name        The name of the provider
+     * @param location    The address including post code
+     * @param phoneNumber The phone number of the provider
+     */
+    public BikeProvider(String name, Location location, String phoneNumber) {
         this.name = name;
-        this.bikesStock = bikeStock;
-        this.partnershipAgreement = partnershipAgreement;
+        this.location = location;
+        this.phoneNumber = phoneNumber;
+        /* bikeStock is initially empty on registration */
+        this.bikeStock = null;
+        
     }
-
-    /* accessors */
-
+    
+    /* getters */
+    public Location getLocation() {
+    	return this.location;
+    }
+    
     /* methods */
-    public checkStock
+    /**
+     * A bike provider can add new bikes into stock
+     * @param newBike A new bike to add to stock
+     */
+    public void addBike(Bike newBike) {
+    	bikeStock.add(newBike);
+    }
+    
+    /**
+     * This method is used to return available bikes for one of the BikeTypes the Customer
+     * searched for using the getQuote method
+     * @param type     A specified type of bike wanted
+     * @param number   The number of bikes wanted for this type
+     * @param dates    The dates for which availability of bikes are checked
+     */
+    public ArrayList<Bike> checkStock(BikeType type, int number, DateRange dates) {
+    	ArrayList<Bike> availableBikes = null;
+    	for (Bike bike : bikeStock) {
+    		if (bike.getBikeType() == type) {
+    			// add the bike to availableBikes if it is available for specified dates
+    		}
+    	}
+    	return availableBikes;
+    }
+    
+    // public makeBooking();
 
-    public makeBooking
+    // public rentOutBikes();
 
-    public rentOutBikes
-
-    public returnBikes
+    public void returnBikes(ArrayList<Bike> bikes) {
+    	
+    }
+    
+    public void makePartnership(BikeProvider partner) {
+    	this.partners.add(partner);
+    }
+    
 }
